50610bb22f386b39c253d9b9c4507145f3f3cc64
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2023-02-24 08:31:22 +0800

    Added install for VSCode

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -27,3 +27,10 @@ git config --global user.email "isaiahcollins_02@live.com"
 # setup oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chsh -s $(which zsh)
+
+# install vscode
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
